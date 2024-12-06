b2b0de9c82d80c94223f55d219f22d986a70d595
Jose Cerqueira <josecerqueira@protonmail.com>
2024-12-06 15:35:06 +0300

    :sparkles: feat: add pool type

--- src/memez/memez.types.ts
+++ src/memez/memez.types.ts
@@ -13,6 +13,12 @@ export enum Network {
   Testnet = 'testnet',
 }
 
+export enum Progress {
+  BONDING = 'bonding',
+  MIGRATING = 'migrating',
+  MIGRATED = 'migrated',
+}
+
 export interface MaybeTx {
   tx?: Transaction;
 }
@@ -56,6 +62,11 @@ export type OwnedObjects = Record<
   string
 >;
 
+export interface Balance {
+  type: string;
+  amount: bigint;
+}
+
 export interface MemezFunConstructorArgs {
   fullNodeUrl?: string;
   packages?: Package;
@@ -74,4 +85,27 @@ export interface NewPumpPoolArgs extends MaybeTx {
   configurationKey: ConfigurationKeys;
   migrationWitness: MigrationWitnesses;
   memeCoinType: string;
+}
+
+interface MemezPool {
+  objectId: string;
+  poolType: string;
+  memeCoinType: string;
+  useTokenStandard: boolean;
+  ipxMemeCoinTreasury: string;
+  metadata: Record<string, string>;
+  migrationWitness: string;
+  progress: Progress;
+}
+
+export interface MemezPumpPool extends MemezPool {
+  devPurchase: Balance;
+  liquidityProvision: Balance;
+  migrationFee: Balance;
+  virtualLiquidity: bigint;
+  targetSuiLiquidity: bigint;
+  suiBalance: Balance;
+  memeBalance: Balance;
+  burnTax: number;
+  swapFee: number;
 }
